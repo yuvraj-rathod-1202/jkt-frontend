a8fea82dafc12a72f6d0411f6bf2ecd016afa118
yuvraj-rathod-1202 <rathodyuvraj12022007>
2024-12-18 10:26:35 +0530

    base url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return "https://jkt-backend-hslouauim-yuvraj-rathods-projects-c5e7e5d3.vercel.app" // backend base url
+    return "https://jkt-backend.vercel.app" // backend base url
 }
 
 export default getBaseUrl;
