0584d207b018b76a76c02ea9b3f5dd97ac5b4f8d
yuvraj-rathod-1202 <rathodyuvraj12022007>
2024-12-18 14:33:37 +0530

    base url

--- src/AdminPages/AdminHome/AdminHome.jsx
+++ src/AdminPages/AdminHome/AdminHome.jsx
@@ -108,7 +108,7 @@ const AdminHome = () => {
       <div>
         <div className="flex flex-col md:col-span-2 md:row-span-2 bg-white shadow rounded-lg">
           <div className="px-6 py-5 font-semibold border-b border-gray-100">
-            The number of orders per month
+            The number of Revenue per Daily
           </div>
           <div className="p-4 flex-grow">
             <div className="flex items-center justify-center h-full px-4 py-16 text-gray-400 text-3xl font-semibold bg-gray-100 border-2 border-gray-200 border-dashed rounded-md">
